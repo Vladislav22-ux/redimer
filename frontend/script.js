5bcf28730abe0bc9dd7a1504346ac662818a8ad1
Vladislav22-ux <vlad453402005@gmail.com>
2025-05-20 15:10:45 +0300

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,6 +0,0 @@
-const API_BASE = '';
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('burgerBtn').addEventListener('click', () => {
-    document.getElementById('mainNav').classList.toggle('visible');
-  });
-});
